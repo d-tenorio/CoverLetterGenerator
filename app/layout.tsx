a556e05e89b93d34c4643f563ad91eab5f5666ad
David Tenorio <dtenorio.work@gmail.com>
2025-01-03 17:39:58 -0700

    add metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Cover Letter Generator',
+  description: 'Generate cover letters at breakneck speed!',
+  icons: { icon: "favicon.ico", apple: "apple-touch-icon.png" }
 }
 
 export default function RootLayout({
